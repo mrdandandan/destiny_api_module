bc02528ec8cf81b5e6ed44333dd7b8ca2aa1b14e
Danny <danny@dannymiller.me>
2016-12-22 15:10:56 -0500

    Endpoint name

--- src/endpoints/user.js
+++ src/endpoints/user.js
@@ -2,7 +2,7 @@ import {PARAMETERS} from '../constants';
 import DestinyApiRequest from '../DestinyApiRequest';
 
 export default {
-    searchDestinyPlayer: new DestinyApiRequest({
+    getBungieAccount: new DestinyApiRequest({
         path: `GetBungieAccount/{${PARAMETERS.MEMBERSHIP_ID}}/{${PARAMETERS.MEMBERSHIP_TYPE}}`,
         routeBinding: `:${PARAMETERS.MEMBERSHIP_ID}/:${PARAMETERS.MEMBERSHIP_TYPE}`,
         requiredParameters: [
